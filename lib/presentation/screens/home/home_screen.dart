f0816009923deefbb97bec1b5014a39268ef07ac
LygiaGarrido <lygiagarrido@hotmail.com>
2024-05-18 15:16:29 +0100

    Add bloc listener to home screen to present toasts after success/error adding new contact

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -4,8 +4,10 @@ import 'package:nimbus_contacts/data/models/contact_model.dart';
 import 'package:nimbus_contacts/logic/cubit/contact/contact_cubit.dart';
 import 'package:nimbus_contacts/logic/cubit/contact/contact_list_cubit.dart';
 import 'package:nimbus_contacts/logic/cubit/contact/contact_list_state.dart';
+import 'package:nimbus_contacts/logic/cubit/contact/contact_state.dart';
 import 'package:nimbus_contacts/presentation/screens/home/texts/home_texts.dart';
 import 'package:nimbus_contacts/presentation/screens/home/widgets/home_body.dart';
+import 'package:nimbus_contacts/presentation/screens/new_contact/new_contact_dialog.dart';
 import 'package:nimbus_contacts/utils/app_color_constants.dart';
 import 'package:nimbus_contacts/utils/utils.dart';
 
@@ -26,45 +28,58 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ContactListCubit, ContactListState>(
-      builder: (context, state) {
-        if (state is ContactListLoadingState) {
-          return Utils.showLoading();
-        } else if (state is ContactListLoadedState) {
-          allItems = state.contacts;
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: appPrimaryColor,
-              title: const Text(
-                appBarTitle,
-                style: TextStyle(color: appYellowColor),
-              ),
-              automaticallyImplyLeading: false,
-            ),
-            body: HomeBody(
-              searchController: searchController,
-              allContactsList: allItems,
-              searchedItemList: items.isEmpty ? allItems : items,
-              onContactTap: (value) {
-                if (value != emptyContact) {
-                  BlocProvider.of<ContactCubit>(context).setContact(value);
-                  Navigator.pushNamed(context, '/contact');
-                }
-              },
-            ),
-            floatingActionButton: FloatingActionButton(
-              backgroundColor: appPrimaryColor,
-              onPressed: () => print('hello!'),
-              child: const Icon(
-                Icons.person_add_alt_1_outlined,
-                color: appYellowColor,
-              ),
-            ),
-          );
-        } else {
-          return const Text('Oops, something happened...');
+    ContactCubit contactCubit = BlocProvider.of<ContactCubit>(context);
+
+    return BlocListener<ContactCubit, ContactState>(
+      listener: (context, state) {
+        if (state is ContactCreatedSuccessState) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              Utils.showCustomSnackBar('You have added a new contact!'));
+        } else if (state is ContactCreatedErrorState) {
+          ScaffoldMessenger.of(context).showSnackBar(Utils.showCustomSnackBar(
+              'Something went wrong, please try again!'));
         }
       },
+      child: BlocBuilder<ContactListCubit, ContactListState>(
+        builder: (context, state) {
+          if (state is ContactListLoadingState) {
+            return Utils.showLoading();
+          } else if (state is ContactListLoadedState) {
+            allItems = state.contacts;
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: appPrimaryColor,
+                title: const Text(
+                  appBarTitle,
+                  style: TextStyle(color: appYellowColor),
+                ),
+                automaticallyImplyLeading: false,
+              ),
+              body: HomeBody(
+                searchController: searchController,
+                allContactsList: allItems,
+                searchedItemList: items.isEmpty ? allItems : items,
+                onContactTap: (value) {
+                  if (value != emptyContact) {
+                    contactCubit.setContact(value);
+                    Navigator.pushNamed(context, '/contact');
+                  }
+                },
+              ),
+              floatingActionButton: FloatingActionButton(
+                backgroundColor: appPrimaryColor,
+                onPressed: () => NewContactDialog.showNewContactDialog(context),
+                child: const Icon(
+                  Icons.person_add_alt_1_outlined,
+                  color: appYellowColor,
+                ),
+              ),
+            );
+          } else {
+            return const Text('Oops, something happened...');
+          }
+        },
+      ),
     );
   }
 
